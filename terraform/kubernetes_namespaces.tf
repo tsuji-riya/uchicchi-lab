ebc9dcdfe006a24aaed5aee1dcac6d05cad179ab
tsuji-riya <tsuji.riya@gmail.com>
2026-07-04 08:13:57 +0900

    update

--- terraform/kubernetes_namespaces.tf
+++ terraform/kubernetes_namespaces.tf
@@ -1,5 +1,17 @@
-// resource "kubernetes_namespace_v1" "cloudflare" {
-//   metadata {
-//     name = "cloudflare"
-//   }
-// }
+resource "kubernetes_namespace_v1" "cloudflare" {
+  metadata {
+    name = "cloudflare"
+  }
+}
+
+resource "kubernetes_namespace_v1" "cockroachdb" {
+  metadata {
+    name = "cockroachdb"
+  }
+}
+
+resource "kubernetes_namespace_v1" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
